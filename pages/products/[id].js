61c8fca305292c652624b6f29f1d6b3dd81410a0
talessio <81319268+talessio@users.noreply.github.com>
2021-11-06 16:08:19 +0100

    carosello ✔️

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -3,6 +3,7 @@ import { getPicture, getAllPictureIds } from "../../lib/pictures";
 import { getAlbum, getAllAlbumIds } from "../../lib/albums"
 import Layout from "../../components/layout";
 import Slider from "../../components/photoSlider"
+import Image from 'next/image'
 
 export async function getStaticPaths() {
     const prodPaths = await getAllProductIds()
@@ -17,26 +18,33 @@ export async function getStaticPaths() {
 export async function getStaticProps(data) {
     const productData = await getProduct(data.params.id)
     // const picData = await getPicture(data.params.id)
-    // const albData = await getAlbum(data.params.id)
+    const albData = await getAlbum(data.params.id)
     return {
         props: {
             productData
             // , picData
-            // , albData
+            , albData
         }
     }
 }
 
 export default function productPage({ productData, picData, albData }) {
+    // console.log(albData)
     return (
         <Layout>
             <main>
                 <h1>{productData.title}</h1>
                 <Slider>
                     {
-                        getAllAlbumIds().map(url => {
+                        albData.map(elem => {
                             return (
-                                <div key={url}>{url}</div>
+                                <div key={elem.id}>
+                                    <Image
+                                        src={elem.url}
+                                        width={500}
+                                        height={500}
+                                    />
+                                </div>
                             )
                         })
                     }
