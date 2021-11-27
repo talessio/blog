36977433e8bad901e9d2e4e0f5329d2f12a82cd1
talessio <81319268+talessio@users.noreply.github.com>
2021-11-27 12:32:28 +0100

    minor changes

--- components/stars.js
+++ components/stars.js
@@ -5,36 +5,41 @@ const StarRating = () => {
     const [rating, setRating] = useState(0)
     const [hover, setHover] = useState(0)
     return (
+        // TotalRateCount(rating),
         <div className={styles.rating}>
             {[...Array(5)].map((star, index) => {
                 index += 1
+                // console.log(index)
                 return (
                     <button
-                    type={styles.button}
-                    key={index}
-                    className={index <= (hover || rating) ? "on" : "off"}
-                    onClick={() => setRating(index)}
-                    onMouseEnter={() => setHover(index)}
-                    onMouseLeave={() => setHover(rating)}
+                        type={styles.button}
+                        key={index}
+                        className={index <= (hover || rating) ? "on" : "off"}
+                        onClick={() => setRating(index)}
+                        onMouseEnter={() => setHover(index)}
+                        onMouseLeave={() => (setHover(rating), TotalRateCount(rating))}
                     >
                         <span
                             className={styles.star}
                             id='star'
-                            >
+                        >
                             ★
                         </span>
                     </button>
                 )
             })}
-        </div>
+        </div >
+
     )
 }
 
-// const FinalRate = () => {
-//     const finalIndex = 6
-//     return (
-//         finalIndex
-//     )
-// }
+function TotalRateCount(rating) {
+    var totalRate
+    totalRate += rating
+    console.log(totalRate)
+    return (
+        totalRate
+    )
+}
 
 export default (StarRating);
